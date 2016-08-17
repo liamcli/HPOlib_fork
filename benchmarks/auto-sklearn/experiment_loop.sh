bc57e68696df32207ffee8ed5da8f27568e311a0
mula0513 <mula0513@gmail.com>
2016-08-16 17:07:56 -0700

    improved experiment_loop.sh

--- benchmarks/auto-sklearn/experiment_loop.sh
+++ benchmarks/auto-sklearn/experiment_loop.sh
@@ -4,7 +4,7 @@
 #Second argument is openml_tid
 #Third argument are seeds
 seeds=("$3")
-export PYTHONPATH=/home/lisha_c_li/HPOlib/
+export PYTHONPATH=$HOME/HPOlib/
 #cd /home/lisha_c_li/HPOlib/benchmarks/auto-sklearn/nocv
 for i in $seeds
 do sudo PYTHONPATH=$PYTHONPATH HPOlib-run -o ../../../optimizers/$1 -s $i --HPOLIB:experiment_directory_prefix open_ml_$2_ --EXPERIMENT:openml_tid $2 
